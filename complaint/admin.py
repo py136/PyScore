a93c77dee7ac1a6c526b4fb54b22406b4c7f84e5
py136 <90089790+py136@users.noreply.github.com>
2026-09-13 18:32:40 +0800

    Delete complaint directory

--- complaint/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Complaint_Score_Details,Complaint_Score_Details_History,Complaint_Score_Details_History_User
-
-admin.site.register(Complaint_Score_Details)
-admin.site.register(Complaint_Score_Details_History)
-admin.site.register(Complaint_Score_Details_History_User)
--- complaint/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComplaintConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'complaint'
