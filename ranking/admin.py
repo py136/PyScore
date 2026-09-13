b800857f631fd6361b1946d2848383f8ebebd09f
py136 <90089790+py136@users.noreply.github.com>
2026-09-13 18:33:36 +0800

    Delete ranking directory

--- ranking/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Ranking_Scores
-
-admin.site.register(Ranking_Scores)
--- ranking/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'ranking'
-
-urlpatterns = [
-    #进行排名
-    path('rank/',views.rank,name='rank'),
-]
